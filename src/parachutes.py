311035007fbf53542a1cf7a67a0ed391c6d0cc79
Christopher Syers <csyers@nd.edu>
2016-04-30 15:10:32 -0400

    game places parachuters at mouse click

--- src/parachutes.py
+++ src/parachutes.py
@@ -9,12 +9,17 @@ import pygame
 from pygame.locals import *
 
 class Parachuter(pygame.sprite.Sprite):
-	def __init__(slef,xpos,ypos,gs=None):
+	def __init__(self,start_pos,gs=None):
 		pygame.sprite.Sprite.__init__(self)
 		self.gs = gs
 		self.image = pygame.image.load("../media/parachute.gif")
+		w,h = self.image.get_size()
+		scale = .5
+		self.image = pygame.transform.scale(self.image, (int(w*scale), int(h*scale)))
 		self.rect = self.image.get_rect()
+		self.rect.center = start_pos
 		self.speed = 2
+		
 		
 	def tick(self):
 		self.rect = self.rect.move(0,self.speed)
@@ -26,12 +31,12 @@ class GameSpace:
 			self.size = self.wifth, self.height = 640, 480
 			self.screen = pygame.display.set_mode(self.size)
 
-			self.bg = pygame.image.load("../media/sky.png")
+			self.bg = pygame.image.load("../media/background.png")
+			self.bg = pygame.transform.scale(self.bg, (640,480))
 			self.clock = pygame.time.Clock()
-			pygame.key.set_repeat(50,100)
 
 			# 2) set up game objects
-
+			self.parachuters = []
 			# 3) start game loop
 			while 1:
 				
@@ -42,12 +47,17 @@ class GameSpace:
 				for event in pygame.event.get():
 					if event.type == QUIT:
 						sys.exit()
-
+					if event.type == MOUSEBUTTONDOWN:
+						self.parachuters.append(Parachuter(pygame.mouse.get_pos(),self))
+						
 				# 6) send a tick to every game object
+				for parachuter in self.parachuters:
+					parachuter.tick()
 
 				# 7) display the game objects
 				self.screen.blit(self.bg,(0,0))
-
+				for parachuter in self.parachuters:
+					self.screen.blit(parachuter.image,parachuter.rect)
 				pygame.display.flip()
 
 if __name__ == '__main__':
