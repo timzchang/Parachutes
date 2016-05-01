ec940ff996217b516d20e5b5d8f149118199aa05
Christopher Syers <csyers@nd.edu>
2016-05-01 13:59:14 -0400

    added bullet class

--- src/parachutes.py
+++ src/parachutes.py
@@ -8,6 +8,23 @@ import sys
 import pygame
 from pygame.locals import *
 
+class Bullet(pygame.sprite.Sprite):
+	def __init__(self,gs=None):
+		pygame.sprite.Sprite.__init__(self)
+		self.gs = gs
+		self.image = pygame.image.load("../media/bullet.png")
+		scale = 1
+		w,h = self.image.get_size()
+		self.image = pygame.transform.scale(self.image, (int(w*scale), int(h*scale)))
+		self.rect = self.image.get_rect()
+		self.rect.center = (300,430)
+		self.out_of_bounds = False
+
+	def tick(self):
+		self.rect = self.rect.move(-1,0)
+		if self.rect.center[1] >= 480 or self.rect.center[1] <= 0 or self.rect.center[0] <= 0 or self.rect.center[0] >= 640:
+			self.out_of_bounds = True
+
 class Turret(pygame.sprite.Sprite):
 	def __init__(self,gs=None):
 		pygame.sprite.Sprite.__init__(self)
@@ -18,6 +35,7 @@ class Turret(pygame.sprite.Sprite):
 		self.image = pygame.transform.scale(self.image, (int(w*scale), int(h*scale)))
 		self.rect = self.image.get_rect()
 		self.rect.center = (300,430)
+		self.out_of_bound = False
 
 	def tick(self):
 		pass
@@ -43,7 +61,7 @@ class Parachuter(pygame.sprite.Sprite):
 		if self.counter == self.speed:
 			self.counter = 0
 			self.rect = self.rect.move(0,self.dy)
-		if self.rect.center[1] >= 447:
+		if self.rect.center[1] >= 450:
 			self.reached_bottom = True
 
 class GameSpace:
@@ -59,6 +77,7 @@ class GameSpace:
 
 			# 2) set up game objects
 			self.parachuters = []
+			self.bullets = []
 			self.turret = Turret()
 			# 3) start game loop
 			while 1:
@@ -68,27 +87,35 @@ class GameSpace:
 
 				# 5) user inputs
 				self.clean_parachuters()
+				self.clean_bullets()
 				for event in pygame.event.get():
 					if event.type == QUIT:
 						sys.exit()
 					if event.type == MOUSEBUTTONDOWN:
 						x = pygame.mouse.get_pos()[0]
 						self.parachuters.append(Parachuter((x, -4),5,self))
+						self.bullets.append(Bullet(self))
 						
 				# 6) send a tick to every game object
 				self.turret.tick()
 				for parachuter in self.parachuters:
 					parachuter.tick()
+				for bullet in self.bullets:
+					bullet.tick()
 
 				# 7) display the game objects
 				self.screen.blit(self.bg,(0,0))
 				self.screen.blit(self.turret.image,self.turret.rect)
 				for parachuter in self.parachuters:
 					self.screen.blit(parachuter.image,parachuter.rect)
+				for bullet in self.bullets:
+					self.screen.blit(bullet.image,bullet.rect)
 				pygame.display.flip()
 
 	def clean_parachuters(self):
 		self.parachuters = [value for value in self.parachuters if value.reached_bottom == False]
+	def clean_bullets(self):
+		self.bullets = [value for value in self.bullets if value.out_of_bounds == False]
 
 
 if __name__ == '__main__':
